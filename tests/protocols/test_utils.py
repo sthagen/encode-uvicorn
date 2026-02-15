e377de40d0a2a1b1d9f55483ee4c1cceb2474502
Marcelo Trylesinski <marcelotryle@gmail.com>
2026-02-15 19:34:14 +0100

    Add socket path to scope["server"] (#2561)
    * Add socket path to scope["server"]
    
    * fix lint
    
    * Address review feedback and update sansio protocol
    
    - Use `if` instead of `elif` after early returns in `get_local_addr`
    - Update `server` type in `WebSocketsSansIOProtocol` to support UDS
    - Add missing test case for full coverage

--- tests/protocols/test_utils.py
+++ tests/protocols/test_utils.py
@@ -10,7 +10,12 @@ from uvicorn.protocols.utils import get_client_addr, get_local_addr, get_remote_
 
 
 class MockSocket:
-    def __init__(self, family, peername=None, sockname=None):
+    def __init__(
+        self,
+        family: socket.AddressFamily,
+        peername: tuple[str, int] | None = None,
+        sockname: tuple[str, int] | str | None = None,
+    ):
         self.peername = peername
         self.sockname = sockname
         self.family = family
@@ -40,9 +45,8 @@ def test_get_local_addr_with_socket():
     transport = MockTransport({"socket": MockSocket(family=socket.AF_INET, sockname=("123.45.6.7", 123))})
     assert get_local_addr(transport) == ("123.45.6.7", 123)
 
-    if hasattr(socket, "AF_UNIX"):  # pragma: no cover
-        transport = MockTransport({"socket": MockSocket(family=socket.AF_UNIX, sockname=("127.0.0.1", 8000))})
-        assert get_local_addr(transport) == ("127.0.0.1", 8000)
+    transport = MockTransport({"socket": MockSocket(family=socket.AF_INET, sockname="/tmp/test.sock")})
+    assert get_local_addr(transport) == ("/tmp/test.sock", None)
 
 
 def test_get_remote_addr_with_socket():
@@ -62,10 +66,13 @@ def test_get_remote_addr_with_socket():
 
 def test_get_local_addr():
     transport = MockTransport({"sockname": "path/to/unix-domain-socket"})
-    assert get_local_addr(transport) is None
+    assert get_local_addr(transport) == ("path/to/unix-domain-socket", None)
 
     transport = MockTransport({"sockname": ("123.45.6.7", 123)})
     assert get_local_addr(transport) == ("123.45.6.7", 123)
+
+    transport = MockTransport({})
+    assert get_local_addr(transport) is None
 
 
 def test_get_remote_addr():
@@ -81,5 +88,5 @@ def test_get_remote_addr():
     [({"client": ("127.0.0.1", 36000)}, "127.0.0.1:36000"), ({"client": None}, "")],
     ids=["ip:port client", "None client"],
 )
-def test_get_client_addr(scope, expected_client):
+def test_get_client_addr(scope: Any, expected_client: str):
     assert get_client_addr(scope) == expected_client
